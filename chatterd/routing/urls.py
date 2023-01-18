1250d1a372c12caa0bea08a5f8f7d264a608fcb4
ngaia01 <95194471+ngaia01@users.noreply.github.com>
2023-01-18 01:41:03 -0500

    Update urls.py

--- chatterd/routing/urls.py
+++ chatterd/routing/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getchatts/', views.getchatts, name='getchatts'),
+    path('postchatt/', views.postchatt, name='postchatt'),
 ]
